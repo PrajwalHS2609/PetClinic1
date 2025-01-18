adccf807ba97603400894c58421688ecbd08b2e4
Prajwal H S <prajwalhs4india@gmail.com>
2025-01-18 19:50:19 +0530

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <link rel="icon" type="image/png" href="./favicon.png" /> */}
+        <link rel="icon" type="image/png" href="./favicon.png" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
